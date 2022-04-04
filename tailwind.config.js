e4419d56525e5227a593662ca30daef0700e610e
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-04-04 10:42:27 +0100

    added Nunito font and some customised colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ module.exports = {
     "./src/**/*.{html,js}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: {
+          50: '#fdf8f6',
+          100: '#f2e8e5',
+          200: '#eaddd7',
+          300: '#e0cec7',
+          400: '#d2bab0',
+          500: '#bfa094',
+          600: '#a18072',
+          700: '#977669',
+          800: '#846358',
+          900: '#43302b',
+        },
+        primary: '#ff6363',
+        secondary: {
+          100:'#e2e2d5',
+          200: '#888883',
+        }
+      },
+      fontFamily: {
+      body: ['Nunito'],
+      }
+    },
   },
   plugins: [],
 }
